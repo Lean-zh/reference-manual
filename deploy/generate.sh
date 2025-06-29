dd20cca3910e33b61859118f73defc59fc5935d1
sinianluoye <sinianluoye@outlook.com>
2025-06-29 14:32:31 +0800

    fix deploy error

--- deploy/generate.sh
+++ deploy/generate.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-lake --quiet exe generate-manual --depth 2 --verbose --without-html-single --output "html"
+# lake --quiet exe generate-manual --depth 2 --verbose --without-html-single --output "html"
+lake --log-level=error exe generate-manual --depth 2 --without-html-single --output "html"
